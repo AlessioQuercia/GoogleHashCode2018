bbbdbb60a572bd25be297779f3dab14462c42982
Alessio Quercia <alessioquercia4@gmail.com>
2019-03-15 10:22:53 +0100

    Version 2 (with different criteria)

--- choice_criteria.c
+++ choice_criteria.c
@@ -8,8 +8,10 @@ void min_wait_time(sample *SMP, int **DIST_S, couple **REW, mixtriple **BEST, in
 	//printf("%d, %f, %d, %d\n", WAIT[idv][idr], BEST[idv][0].val, t, DIST_S[idv][idr] + SMP->rides[idr].dd);
 
 	// If the wait_time of assigning ride r to vehicle v is smaller than the one in the current slot, update it with the smaller one
-	if ( ((WAIT[idv][idr] < BEST[idv][0].val) || (WAIT[idv][idr] == BEST[idv][0].val && required_time < BEST[idv][0].val)) 
-		&& required_time <= SMP->T)
+	if ( ((WAIT[idv][idr] < BEST[idv][0].val)																									// first criterium: minimum wait_time
+		|| (WAIT[idv][idr] == BEST[idv][0].val && REW[idv][idr].val > REW[idv][BEST[idv][0].idr].val)											// second criterium: maximum reward
+		|| (WAIT[idv][idr] == BEST[idv][0].val && REW[idv][idr].val == REW[idv][BEST[idv][0].idr].val && required_time < BEST[idv][0].val))		// third criterium: minimum required_time
+		&& required_time <= SMP->T )
 	{
 		mixtriple mc = { .val = WAIT[idv][idr], .idr = idr , .t = required_time};
 
